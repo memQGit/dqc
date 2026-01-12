6f9f51047cae3a1c2c36ce7f6baf7f2257207361
LukeAndreesen <107073823+LukeAndreesen@users.noreply.github.com>
2026-01-12 13:23:06 -0600

    basic ci/cd setup

--- src/main.py
+++
@@ -1,8 +0,0 @@
-def main():
-    print(
-        "i am making a line that is too long to check the linter!!!!!!!!!!!!!!!!!!!"
-    )
-
-
-if __name__ == "__main__":
-    main()
---
+++ src/memq_dqc/main.py
@@ -0,0 +1,8 @@
+def main():
+    print(
+        "Welcome to memq_dqc! This is the main entry point of the application."
+    )
+
+
+if __name__ == "__main__":
+    main()
