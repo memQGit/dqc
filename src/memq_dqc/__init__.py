c5c43c76f70308b2c63a9c20108cb239e76f5a1a
LukeAndreesen <107073823+LukeAndreesen@users.noreply.github.com>
2026-01-14 08:24:57 -0600

    Basic library skeleton constructed

--- src/memq_dqc/__init__.py
+++ src/memq_dqc/__init__.py
@@ -1,2 +1,33 @@
+# ============================================================================
+# Copyright (c) 2026 memQ Inc.
+#
+# This source code is licensed under the MIT License.
+# See the LICENSE file in the project root for full license information.
+# ============================================================================
+
+"""A one-line summary of the module or program, terminated by a period.
+
+Leave one blank line.  The rest of this docstring should contain an
+overall description of the module or program.  Optionally, it may also
+contain a brief description of exported classes and functions and/or usage
+examples.
+
+Typical usage example:
+
+  foo = ClassFoo()
+  bar = foo.function_bar()
+"""
+
+
 def hello() -> str:
+    """Example function with PEP 484 type annotations.
+
+    Args:
+        param1: The first parameter.
+        param2: The second parameter.
+
+    Returns:
+        The return value. True for success, False otherwise.
+
+    """
     return "Hello from memq-dqc!"
